b061ae1fc380ae5e1270159b350f79fa0b564a64
Rebeca  Freitas <rebecasguerri@gmail.com>
2024-01-06 23:34:30 -0300

    update

---
+++ overlay.js
@@ -0,0 +1,13 @@
+const overlay = document.querySelector('.overlay')
+window.addEventListener('mousemove', (e) => {
+    const {clientX, clientY} = e
+    const x = Math.round((clientX / window.innerWidth) * 100);
+    const y = Math.round((clientY / window.innerHeight) * 100);
+
+    gsap.to(overlay, {
+        '--x': `${x}%`,
+        '--y': `${y}%`,
+        duration:0.3,
+        ease: 'sine.out'
+    })
+})
